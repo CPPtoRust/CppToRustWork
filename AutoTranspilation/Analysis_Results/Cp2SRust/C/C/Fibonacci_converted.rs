3cf715a9d0d40ae73d36352851d3511580ee1f7a
CPPtoRust <130639442+CPPtoRust@users.noreply.github.com>
2024-06-26 22:48:54 +0530

    Add files via upload

---
+++ AutoTranspilation/Analysis_Results/Cp2SRust/C/C/Fibonacci_converted.rs
@@ -0,0 +1,12 @@
+fn fib(mut n: i32) -> i32 {
+    if n <= 1 {
+        return n;
+    }
+    return fib(n - 1) + fib(n - 2);
+}
+fn main() {
+    let mut n: i32 = 9;
+    println!("{}", fib(n));
+    getchar();
+    return 0;
+}
